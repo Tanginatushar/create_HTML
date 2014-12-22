8537e672f4295d0c456ebf58707c3d35e17f1c95
ahmed fahad <f20_777@yahoo.com>
2014-12-22 14:35:02 +0600

    for _post DB info

--- add.php
+++ add.php
@@ -1,4 +1,15 @@
 <?php
 print_r($_REQUEST);
 
-?>
+$link =mysqli_connect("localhost","root","lict@2","students") ;
+$query ="INSERT INTO `students`.`users` (
+`first_name` ,
+`last_name`
+)
+
+VALUES(
+'".$_POST['firstName']."', '".$_POST['LastName']."'
+                                        );";
+echo $query;
+
+mysqli_query($link,$query);
